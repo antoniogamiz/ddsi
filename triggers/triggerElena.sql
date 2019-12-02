9b49163caf42f4c36844bae12089e18a4ed7c50d
Elena Merelo <elenamerelo@correo.ugr.es>
2019-12-02 13:14:49 +0100

    Update triggerElena.sql

--- triggers/triggerElena.sql
+++ triggers/triggerElena.sql
@@ -1,12 +1,20 @@
 USE ddsi;
 
-create TRIGGER dni_unico
+create TRIGGER dni_no_unico
 before INSERT or UPDATE ON Empleado
-FOR EACH ROW
+# FOR EACH ROW
     DECLARE 
         dni_unico EXCEPTION;
+        PRAGMA EXCEPTION_INIT (dni_no_unico, -1);
     BEGIN
-    IF NOT EXISTS (SELECT * FROM Empleado AS e WHERE e.DNI =:new.DNI)
+    IF EXISTS (SELECT * FROM Empleado AS e WHERE e.DNI =:new.DNI)
     THEN
-        RAISE dni_unico;
+        RAISE dni_no_unico;
     END_IF;
+    
+    EXCEPTION
+        WHEN dni_no_unico THEN
+            DBMS_OUTPUT.PUT_LINE (“Error: ya hay un empleado con ese DNI en el sistema”); 
+            RAISE DUP_VAL_ON_INDEX;
+        END;
+END;
