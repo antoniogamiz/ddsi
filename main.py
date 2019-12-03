301b0e132cebc7ed5f945e0070420f16d69fb5d0
Antonio Gamiz <antoniogamiz10@gmail.com>
2019-12-03 10:55:43 +0100

    añade algunas filas a la base de datos y prepara main.py

--- main.py
+++ main.py
@@ -1,35 +1,29 @@
 from tkinter import *
-import pymysql as mdb
+import pymysql
 from tkinter import messagebox
-import re
-import os
-
-# Definimos una clase para la interfaz
 
 
-class Aplicacion(object):
-    def __init__(self, ventana, user, pswd):
+class App(object):
+    def __init__(self, window, user, pswd):
 
-        # Datos para conectarse a la base de datos
+        # db connection
         self.user = user
         self.pswd = pswd
 
-        self.peticion_realizada = False
+        self.connection = pymysql.connect(
+            'localhost', self.user, self.pswd, 'ddsi')
+        self.cursor = self.connection.cursor()
 
-        conexion = mdb.connect('localhost', self.user, self.pswd, 'ddsi')
-        self.cursor = conexion.cursor()
-
-        # Ponemos el título a la ventana
-        ventana.wm_title("3.14-zita")
+        window.wm_title("3.14-zita")
 
 
 def main(argv):
     user = argv[1]
     pswd = argv[2]
 
-    ventana = Tk()
-    Aplicacion(ventana, user, pswd)
-    ventana.mainloop()
+    window = Tk()
+    App(window, user, pswd)
+    window.mainloop()
 
 
 if __name__ == "__main__":
--- objetos.sql
+++ objetos.sql
@@ -1,46 +1,9 @@
 USE ddsi;
 
-INSERT INTO Universo VALUES ("Universo 1", "SciFi", "reglas1.txt");
-INSERT INTO Universo VALUES ("Universo 2", "Fantasía", "reglas2.txt");
-INSERT INTO Universo VALUES ("Universo 3", "Medieval", "reglas3.txt");
+-- Elena
+-- Antonio
+INSERT INTO Producto VALUES ("tomate" , 100, '2020/12/27', "mercadona", 200 );
+INSERT INTO Producto VALUES ("pepino" , 100, '2020/11/17', "lidl"     , 100 );
+INSERT INTO Producto VALUES ("cebolla", 50 , '2020/10/05', "carrefour", 200 );
+INSERT INTO Producto VALUES ("queso"  , 10 , '2020/09/03', "coviran"  , 50  );
 
-INSERT INTO Jugador VALUES ("12345678A", "Willyrex", "willyrex@hotmail.com");
-INSERT INTO Jugador VALUES ("12345679B", "Rubius", "rubius@hotmail.es");
-INSERT INTO Jugador VALUES ("12345672Z", "Yellowmellow", "yellow@gmail.com");
-
-INSERT INTO Personaje (nombre, atributos, estado, jug_dni, uni_id)
-  VALUES ("W1lly", "Magia", 0, "12345678A", "Universo 1");
-INSERT INTO Personaje (nombre, atributos, estado, jug_dni, uni_id)
-  VALUES ("W2lly", "Agilidad", 1, "12345678A", "Universo 2");
-INSERT INTO Personaje (nombre, atributos, estado, jug_dni, uni_id)
-  VALUES ("W3lly", "Fuerza", 0, "12345678A", "Universo 1");
-
-INSERT INTO Personaje (nombre, atributos, estado, jug_dni, uni_id)
-  VALUES ("Rub1us", "Alquimia", 0, "12345679B", "Universo 2");
-INSERT INTO Personaje (nombre, atributos, estado, jug_dni, uni_id)
-  VALUES ("Rubi2s", "Arquería", 0, "12345679B", "Universo 3");
-
-INSERT INTO Personaje (nombre, atributos, estado, jug_dni, uni_id)
-  VALUES ("Yel1ow", "Sigilo", 1, "12345672Z", "Universo 3");
-
-INSERT INTO Partida (nombre, log_fechas, uni_nombre)
-  VALUES ("Partida 1", "log1", "Universo 1");
-INSERT INTO Partida (nombre, log_fechas, uni_nombre)
-  VALUES ("Partida 2", "log2", "Universo 2");
-INSERT INTO Partida (nombre, log_fechas, uni_nombre)
-  VALUES ("Partida 3", "log3", "Universo 2");
-INSERT INTO Partida (nombre, log_fechas, uni_nombre)
-  VALUES ("Partida 4", "log4", "Universo 3");
-
-INSERT INTO Rolea VALUES ("12345678A", 1);
-INSERT INTO Rolea VALUES ("12345678A", 2);
-INSERT INTO Rolea VALUES ("12345679B", 2);
-INSERT INTO Rolea VALUES ("12345679B", 3);
-INSERT INTO Rolea VALUES ("12345679B", 4);
-INSERT INTO Rolea VALUES ("12345672Z", 4);
-
-INSERT INTO Participa VALUES (1, 1);
-INSERT INTO Participa VALUES (2, 2);
-INSERT INTO Participa VALUES (4, 2);
-INSERT INTO Participa VALUES (5, 3);
-INSERT INTO Participa VALUES (6, 4);
