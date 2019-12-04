780cab9108b42a141831aba1482cbedf9a28f7e7
Elena Merelo <elenamerelo@correo.ugr.es>
2019-12-04 11:38:53 +0100

    Update objetos.sql

--- objetos.sql
+++ objetos.sql
@@ -1,6 +1,17 @@
 USE ddsi;
 
 -- Elena
+INSERT INTO PuestoTrabajo VALUES ("camarero" , 800, 45, "horario1");
+INSERT INTO PuestoTrabajo VALUES ("chef" , 900, 45, "horario1");
+INSERT INTO PuestoTrabajo VALUES ("repartidor", 800, 45 , "horario1");
+INSERT INTO PuestoTrabajo VALUES ("encargado", 1000, 40 , "horario1");
+
+INSERT INTO Empleado VALUES ("12345678A" , "Antonio Molina", 1050, "encargado");
+INSERT INTO Empleado VALUES ("87654321B" , "Francisca Garcia", 950, "chef");
+INSERT INTO Empleado VALUES ("13572468C", "Pancho Gamuzo" , 806, "camarero");
+INSERT INTO Empleado VALUES ("24681357D"  , "Pepe Motillos" , 825, "repartidor");
+
+
 -- Antonio
 INSERT INTO Producto VALUES ("tomate" , 100, '2020/12/27', "mercadona", 200 );
 INSERT INTO Producto VALUES ("pepino" , 100, '2020/11/17', "lidl"     , 100 );
