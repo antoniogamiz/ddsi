552b95072de71f3a196d24c13450d7f115eeba07
Laura Sánchez <sanchezparra.laura@gmail.com>
2019-12-13 17:35:21 +0100

    Laura´s trigger (waiting for antonio to check)

--- triggers/triggerLaura.sql
+++ triggers/triggerLaura.sql
@@ -1,33 +1,11 @@
 USE ddsi;
 
 DELIMITER |
-DROP TRIGGER IF EXISTS ComprobarExistenciaUniversoINSERT |
-CREATE TRIGGER ComprobarExistenciaUniversoINSERT BEFORE INSERT ON Personaje
+DROP TRIGGER IF EXISTS noExisteIngrediente
+CREATE TRIGGER noExisteIngrediente BEFORE INSERT ON Plato
   FOR EACH ROW
   BEGIN
-    CALL ComprobarExistenciaUniverso(NEW.uni_id);
-  END |
-DELIMITER ;
-
-DELIMITER |
-DROP TRIGGER IF EXISTS ComprobarExistenciaUniversoUPDATE |
-CREATE TRIGGER ComprobarExistenciaUniversoUPDATE BEFORE UPDATE ON Personaje
-  FOR EACH ROW
-  BEGIN
-    CALL ComprobarExistenciaUniverso(NEW.uni_id);
-  END |
-DELIMITER ;
-
-DELIMITER |
-DROP PROCEDURE IF EXISTS ComprobarExistenciaUniverso |
-CREATE PROCEDURE ComprobarExistenciaUniverso(IN nuevoNombre VARCHAR(32))
-READS SQL DATA
-  BEGIN
-    DECLARE msg varchar(124);
-    IF NOT EXISTS (SELECT * FROM Universo AS uni WHERE uni.nombre = nuevoNombre) THEN
-      SET msg = concat('Error: El nombre de universo "', cast(nuevoNombre as char));
-      SET msg = concat(msg, '", no existe.');
-      SIGNAL SQLSTATE '45000' SET message_text = msg;
-    END IF;
-  END |
+ 	IF NOT EXISTS (SELECT * FROM Producto as a WHERE a.nombre = NEW.ingredient) THEN
+		signal sqlstate '45000' SET message_text = 'El ingrediente no se encuentra disponible';
+END;//
 DELIMITER ;
